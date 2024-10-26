1f2e666cf1fb5c7311872a23a9d7e66c1a74ea34
Ivan Busygin <bi-25@yandex.ru>
2024-10-27 03:25:18 +0400

    init project

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default tsEslint.config(
+  { files: ['**/*.{ts}'] },
+  pluginJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2023 },
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslint.plugin,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      '@typescript-eslint/no-unused-vars': 1,
+    },
+    ignores: ['node_modules', 'eslint.config.js', 'dist', 'front'],
+  },
+);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { httpServer } from './http_server';
+
+const HTTP_PORT = 8181;
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Start static http server on the ${HTTP_PORT} port!`);
+});
